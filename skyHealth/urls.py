444e9df145fde7cd3f3a325d5b4f5d8d4fdc6416
DiegoSantos07 <diegosantosfreitas7@hotmail.com>
2025-04-14 15:11:32 +0100

    Initial structure of login page
    Created the structure of login page

--- skyHealth/urls.py
+++ skyHealth/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.login,),
 ]
