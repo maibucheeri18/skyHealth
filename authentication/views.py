c5f533da5c4fb59feb828d458837d207f9b78c88
DiegoSantos07 <diegosantosfreitas7@hotmail.com>
2025-04-20 23:06:01 +0100

    Addition of page requests on views.py
    Removed upper case "Q" to "q" to keep consistency for files and find page easier when testing URL on browser. New requests added due to the creation of new pages.

--- authentication/views.py
+++ authentication/views.py
@@ -7,4 +7,13 @@ def login(request):
     return render(request, 'login.html')
 
 def security_questions(request):
-    return render(request, 'securityQuestions.html')
+    return render(request, 'securityquestions.html')
+
+def reset_password(request):
+    return render(request, 'resetpassword.html')
+
+def create_account(request):
+    return render(request, 'createaccount.html')
+
+def security_questions2(request):
+    return render(request, 'securityquestions2.html')
