628756266e7b46188e020828178841d57d4e7160
Annie <w1892798@my.westminster.ac.uk>
2025-04-22 12:38:23 +0100

    Change views.py to make the start page viewable

--- healthCheck/views.py
+++ healthCheck/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def votingForm(request):
-    return render(request, 'votingForm.html')
+def startPage(request):
+    return render(request, 'startPage.html')
