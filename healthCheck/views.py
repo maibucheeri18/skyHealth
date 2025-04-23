43047e1f42ba88f0ea9f7e2fab32ae2573cd065b
Annie <w1892798@my.westminster.ac.uk>
2025-04-23 15:12:21 +0100

    Add url paths for the choose session and choose team pages in urls.py

--- healthCheck/views.py
+++ healthCheck/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def chooseSession(request):
+    return render(request, 'chooseSession.html')
+
+def chooseTeam(request):
+    return render(request, 'chooseTeam.html')
+
 def startPage(request):
     return render(request, 'startPage.html')
 
