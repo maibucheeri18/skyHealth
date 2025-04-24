0454259a714a6b9e6024553a264d096e879225af
DiegoSantos07 <diegosantosfreitas7@hotmail.com>
2025-04-24 11:28:53 +0100

    Update urls.py
    Added name='login' in order to match naming conventions and corrected indentation on line 27.

--- skyHealth/urls.py
+++ skyHealth/urls.py
@@ -20,9 +20,9 @@ from authentication import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.login,),
+    path('login/', auth_views.login, name='login'),
     path('securityquestions/', auth_views.security_questions, name='security_questions'),
     path('resetpassword/', auth_views.reset_password, name='reset_password'),
     path('createaccount/', auth_views.create_account, name='create_account'),
-     path('securityquestions2/', auth_views.security_questions2, name='security_questions2')
+    path('securityquestions2/', auth_views.security_questions2, name='security_questions2')
 ]
