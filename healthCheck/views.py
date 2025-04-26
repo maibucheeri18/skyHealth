7ae1bb7492caccbf282155ee5b78b28d1c9d462b
Annie <w1892798@my.westminster.ac.uk>
2025-04-26 22:49:10 +0100

    Retrieve objects for Session and Team from the database
    I am now starting to do JS and back-end for my work, also fixed a typo for healthCheckForm

--- healthCheck/views.py
+++ healthCheck/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from database.models import Session
+from database.models import Team
 
 # Create your views here.
 def chooseSession(request):
-    return render(request, 'chooseSession.html')
+    sessions = Session.objects.all()   #retrieving all session objects
+    return render(request, 'chooseSession.html', {'sessions': sessions})
 
 def chooseTeam(request):
-    return render(request, 'chooseTeam.html')
+    teams = Team.objects.all()
+    return render(request, 'chooseTeam.html', {'teams': teams})
 
 def startPage(request):
     return render(request, 'startPage.html')
 
-def healthcheckForm(request):
-    return render(request, 'healthcheckForm.html')
+def healthCheckForm(request):
+    return render(request, 'healthCheckForm.html')
 
 def closingPage(request):
     return render(request, 'closingPage.html')
