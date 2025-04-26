4f4fef6930eddf1d67e72865fc41b369c4b068af
DiegoSantos07 <diegosantosfreitas7@hotmail.com>
2025-04-26 21:07:36 +0100

    Update forms.py
    Added forms for respectives  html which will collect data to databse.

--- authentication/forms.py
+++ authentication/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'Username or email'})
@@ -9,6 +10,7 @@ class LoginForm(forms.Form):
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'Password'})
     )
+
 
 class CreateAccountForm(forms.ModelForm):
     first_name = forms.CharField(
@@ -26,14 +28,56 @@ class CreateAccountForm(forms.ModelForm):
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'Create your password'})
     )
-    
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'username', 'password')
-        
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password"])
         if commit:
             user.save()
         return user
+
+
+class SetSecurityQuestionsForm(forms.Form):
+    city = forms.CharField(
+        label="What is the name of the city you were born in?",
+        widget=forms.TextInput(attrs={'placeholder': 'City'})
+    )
+    company = forms.CharField(
+        label="What is the name of the first company you worked for?",
+        widget=forms.TextInput(attrs={'placeholder': 'First job company'})
+    )
+
+
+class CheckSecurityQuestionsForm(forms.Form):
+    city = forms.CharField(
+        label="What is the name of the city you were born in?",
+        widget=forms.TextInput(attrs={'placeholder': 'City'})
+    )
+    company = forms.CharField(
+        label="What is the name of the first company you worked for?",
+        widget=forms.TextInput(attrs={'placeholder': 'First job company'})
+    )
+
+
+class ResetPasswordForm(forms.Form):
+    new_password = forms.CharField(
+        label="Enter new password",
+        widget=forms.PasswordInput(attrs={'placeholder': 'New password'})
+    )
+    confirm_password = forms.CharField(
+        label="Confirm new password",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'})
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("new_password")
+        confirm = cleaned_data.get("confirm_password")
+
+        if password and confirm and password != confirm:
+            raise forms.ValidationError("Passwords do not match.")
+        return cleaned_data
