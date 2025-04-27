8af047a1643e6f63025cc9508d6a1ff36f26c311
maibucheeri18 <bucheeri.mai@gmail.com>
2025-04-27 13:37:52 +0100

    feat: adapt user's search filter in function based on logged-in user

--- results/views.py
+++ results/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from skyHealth.utils import get_user_level_context
 
 # Create your views here.
 # results page
+@login_required
 def results(request):
-    return render(request, 'results.html')
+    # the following is regarding adapting the search filter based on the user logged in
+    context = get_user_level_context(request)
+
+    user_level = context.get('user_level', 0)
+
+    context.update({
+        'show_engineer_filters': user_level == 0,
+        'show_team_leader_filters': user_level == 1,
+        'show_department_leader_filters': user_level == 2,
+        'show_senior_manager_filters': user_level == 3,
+    })
+
+    return render(request, 'results.html', context)
