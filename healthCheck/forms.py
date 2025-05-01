ce816ef2605927cb3b62a930a2985ebc5a7a618e
maibucheeri18 <bucheeri.mai@gmail.com>
2025-05-01 08:18:27 +0100

    fix: debug and resolve errors

--- healthCheck/forms.py
+++ healthCheck/forms.py
@@ -36,7 +36,7 @@ class VoteForm(forms.ModelForm):
 
     #field for user comment
     voteComment = forms.CharField(
-        required=True,
+        required=False,
         widget = forms.Textarea(attrs={
             'id': 'comment',
             'placeholder': 'Add your comment'
